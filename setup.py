59bda260ba38fc5320df00190fe60e08fafb422b
Tricky <riosandres294@gmail.com>
2025-07-29 13:01:19 -0500

    I dont know anymore

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="releasesPractice",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Andres Rios",                         # Tu nombre
+    author_email="riosandres294@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/Andres-pixel35/releasesPractice.git",     # URL del proyecto
+)
